af005ef135f8616e68e5e9c386922c120d163651
Andrew Stanish <andy@youshallthrive.com>
2016-05-15 22:31:58 -0400

    sqlalchemy event thing

--- generators/app/templates/manage.py
+++ generators/app/templates/manage.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager
 from flask.ext.script.commands import ShowUrls, Clean
 
 from <%= appName %> import create_app<% if (databaseMapper === 'sqlalchemy') { -%>, db<% } %>
@@ -11,7 +11,6 @@ from <%= appName %> import create_app<% if (databaseMapper === 'sqlalchemy') { -
 app = create_app(os.getenv('<%= appEnvVar %>_CONFIG', 'default'))
 
 manager = Manager(app)
-manager.add_command("server", Server())
 manager.add_command("show-urls", ShowUrls())
 manager.add_command("clean", Clean())
 
